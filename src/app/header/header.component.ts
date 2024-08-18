d7e0308948b13f69ab05454e1a5b51e6dadf64b0
madhu dharani <cmdharani@outlook.com>
2024-08-18 22:42:21 +0530

    Angular Project implementation

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html',
+    styleUrls:['./header.component.css']
+    //styles:['a{text-decoration:none; margin:0 10px;}']
+    // template:'<h3>eKart by Dharani</h3>'
+})
+export class HeaderComponent{
+
+}
