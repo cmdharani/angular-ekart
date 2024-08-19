1b8760eb5b72244b68c0fc88378814f993fe2a96
madhu dharani <cmdharani@outlook.com>
2024-08-19 13:57:41 +0530

    globall CSS and install bootstrap

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,7 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-footer',
-    template:'<h1>Footer Component <\h1>'
+    templateUrl:'./footer.component.html',
+    styleUrls:['./footer.component.css']
+    //template:'<h1>Footer Component <\h1>'
 })
 
 export class FooterComponent
