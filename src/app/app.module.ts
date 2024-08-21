1db38b189cbf11d15d8142c8ff1b0d2f7716520f
madhu dharani <cmdharani@outlook.com>
2024-08-21 22:51:42 +0530

    renaming the files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import { FooterComponent } from './footer/footer.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { SearchComponent } from './product-list/search/search.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -21,7 +21,7 @@ import { FormsModule } from '@angular/forms';
     TopHeaderComponent,
     TopMenuComponent,
     MainMenuComponent,
-    ProductListComponent,
+    ContainerComponent,
     SearchComponent
   ],
   imports: [
