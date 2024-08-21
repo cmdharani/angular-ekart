f8c712f1ea84d71f875ecb23d968832be4ab2ad2
madhu dharani <cmdharani@outlook.com>
2024-08-21 22:30:30 +0530

    ngFor Directive

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,23 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
- name:string="IPhone 15";
- price:number=999;
- color:string="Red";
 
- IphoneProduct={
-    name:"IPhone 7",
-    price:300,
-    color:"Red",
-    Discount:9.5,
-    itemStock:5,
-    pImage:'assets/images/IphoneX.png'
- }
+//  name:string="IPhone 15";
+//  price:number=999;
+//  color:string="Red";
+
+//  IphoneProduct={
+//     name:"IPhone 7",
+//     price:300,
+//     color:"Red",
+//     Discount:9.5,
+//     itemStock:5,
+//     pImage:'assets/images/IphoneX.png'
+//  }
 
 
- getDiscountedPrice()
- {
-  return this.IphoneProduct.price-(this.IphoneProduct.price * this.IphoneProduct.Discount/100)
- }
+//  getDiscountedPrice()
+//  {
+//   return this.IphoneProduct.price-(this.IphoneProduct.price * this.IphoneProduct.Discount/100)
+//  }
+
+
+listOfString:string[] =['Bangalore','Hassan','Mangalore','Chitradurga']
 
 }
